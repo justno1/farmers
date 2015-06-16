825b433f20f2358e19dffbde578627bd1822619b
justno1 <justno1@hotmail.com.tw>
2015-06-16 11:31:34 +0800

    first commit farmer try

---
+++ exercise/js/farmer_story.js
@@ -0,0 +1,41 @@
+(function(){
+
+	Parse.initialize("lb68hfMRYgqhmuro8Ook5xux5mORCjF59w6Exfek","PAQaaFUhvx52GXTHFsZ4MeLF2aaQNrJT1xx6je2S");
+
+    document.getElementById('btnSubmit').addEventListener("click",function(){
+
+    	alert('something');
+		var Crop = Parse.Object.extend("crop");
+		var crop = new Crop();
+/*
+		crop.set("cropName",document.getElementById('crop').value);
+		crop.set("farmerName","somebody");
+		crop.set("feature",document.getElementById('feature').value);
+		crop.set("seedingTime",document.getElementById('start_time').value);
+		crop.set("sellingTime",document.getElementById('sell_date').value);
+		crop.set("seedingPosition",document.getElementById('location').value);
+		crop.set("cropImageUrl",document.getElementById('photoUrl').value);
+*/
+		crop.save({
+			cropName:"no",
+			farmerName:"no",
+			feature:"no",
+			seedingTime:"no",
+			sellingTime:"no",
+			seedingPosition:"no",
+			cropImageUrl:"no",
+		},{
+		  success: function(crop) {
+		    // Execute any logic that should take place after the object is saved.
+		    alert('New object created with objectId: ' + crop.id);
+		  },
+		  error: function(crop, error) {
+		    // Execute any logic that should take place if the save fails.
+		    // error is a Parse.Error with an error code and message.
+		    console.log(error.message);
+		    alert('Failed to create new object, with error code: ' + error.message);
+		  }
+		});
+	});
+
+}());
