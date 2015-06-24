8d535f3a153039988eea06ee67c67db1c300951c
justno1 <justno1@hotmail.com.tw>
2015-06-24 21:16:19 +0800

    test

--- exercise/js/farmer_story.js
+++ exercise/js/farmer_story.js
@@ -12,7 +12,7 @@
 		crop.set("cropName",document.getElementById('crop').value);
 		crop.set("farmerName","somebody");
 		crop.set("feature",document.getElementById('feature').value);
-		crop.set("seedingTime",document.getElementById('start_time').value);
+	//	crop.set("seedingTime",document.getElementById('start_time').value);
 	//	crop.set("sellingTime",document.getElementById('sell_date').value);
 		crop.set("seedingPosition",document.getElementById('location').value);
 		crop.set("cropImageUrl",document.getElementById('photoUrl').value);
